f11e3aede1b38f51953f7ccf127e1a86230511c9
karimbipbop <karim.el.bair@estudiantat.upc.edu>
2024-12-23 14:12:42 +0100

    sdf

--- PassarelaUsuari.cpp
+++ PassarelaUsuari.cpp
@@ -1,39 +1,7 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include "ConnexioBD.h"
+#include "PassarelaUsuari.h"
 
-
-
-using namespace std;
-
-const int SobrenomExisteix = 3;
-const int CorreuExisteix = 4;
-
-class PassarelaUsuari {
-public:
-    PassarelaUsuari(sql::ResultSet* result);
-    PassarelaUsuari();
-    PassarelaUsuari(string nomU, string sobrenomU, string contrasenyaU, string correuElectronicU, string dataNaixementU, string subscripcioU);
-    void insereix();
-    void modifica();
-    void esborra();
-
-    string obteContrasenya();
-    string obteNom();
-    string obteSobrenom();
-    string obteCorreuElectronic();
-    string obteDataNaixement();
-    void posaContrasenya(string contraU);
-    void posaNom(string nomU);
-    void posaCorreuElectronic(string correuU);
-    void posaDataNaixement(string neixU);
-
-private:
-    string nom, sobrenom, contrasenya, correuElectronic, dataNaixement, subscripcio;
-};
-
-PassarelaUsuari::PassarelaUsuari() {
+PassarelaUsuari::PassarelaUsuari()
+{
     nom = "";
     sobrenom = "";
     contrasenya = "";
@@ -42,7 +10,8 @@ PassarelaUsuari::PassarelaUsuari() {
     subscripcio = "";
 }
 
-PassarelaUsuari::PassarelaUsuari(sql::ResultSet* result) {
+PassarelaUsuari::PassarelaUsuari(sql::ResultSet* result)
+{
     if (result->next()) {
         sobrenom = result->getString("sobrenom");
         nom = result->getString("nom");
@@ -54,8 +23,8 @@ PassarelaUsuari::PassarelaUsuari(sql::ResultSet* result) {
 }
 
 
-PassarelaUsuari::PassarelaUsuari(string nomU, string sobrenomU, string contrasenyaU, string correuElectronicU, string dataNaixementU, string subscripcioU) {
-
+PassarelaUsuari::PassarelaUsuari(string nomU, string sobrenomU, string contrasenyaU, string correuElectronicU, string dataNaixementU, string subscripcioU)
+{
     nom = nomU;
     sobrenom = sobrenomU;
     contrasenya = contrasenyaU;
@@ -64,7 +33,8 @@ PassarelaUsuari::PassarelaUsuari(string nomU, string sobrenomU, string contrasen
     subscripcio = subscripcioU;
 }
 
-void PassarelaUsuari::insereix() {
+void PassarelaUsuari::insereix()
+{
     try {
         ConnexioBD& c = ConnexioBD::getInstance();
         std::string query = "INSERT INTO usuari VALUES ('" + 
@@ -80,7 +50,8 @@ void PassarelaUsuari::insereix() {
 	}
 }
 
-void PassarelaUsuari::modifica() {
+void PassarelaUsuari::modifica()
+{
     try {
         ConnexioBD& c = ConnexioBD::getInstance();
         std::string query = "UPDATE usuari SET "
@@ -92,11 +63,12 @@ void PassarelaUsuari::modifica() {
         c.execucio(query);
     }
     catch (sql::SQLException& e) {
-		std::cerr << "SQL Error: " << e.what() << std::endl;
+		std::cerr << "SQL Error: " << e.what() << endl;
 	}
 }
 
-void PassarelaUsuari::esborra() {
+void PassarelaUsuari::esborra()
+{
     try {
         ConnexioBD& c = ConnexioBD::getInstance();
         std::string query = "DELETE FROM usuari WHERE sobrenom = '" + sobrenom + "'";
@@ -107,47 +79,48 @@ void PassarelaUsuari::esborra() {
 	}
 }
 
-string PassarelaUsuari::obteContrasenya() {
+string PassarelaUsuari::obteContrasenya()
+{
     return contrasenya;
 }
 
 
-string PassarelaUsuari::obteNom() {
+string PassarelaUsuari::obteNom()
+{
     return nom;
 }
 
-string PassarelaUsuari::obteSobrenom() {
+string PassarelaUsuari::obteSobrenom()
+{
     return sobrenom;
 }
 
-string PassarelaUsuari::obteCorreuElectronic() {
+string PassarelaUsuari::obteCorreuElectronic()
+{
     return correuElectronic;
 }
 
-string PassarelaUsuari::obteDataNaixement() {
+string PassarelaUsuari::obteDataNaixement()
+{
     return dataNaixement;
 }
 
-void PassarelaUsuari::posaContrasenya(string contraU) {
-
+void PassarelaUsuari::posaContrasenya(string contraU)
+{
     contrasenya = contraU;
-
 }
 
-void PassarelaUsuari::posaNom(string nomU) {
-
+void PassarelaUsuari::posaNom(string nomU)
+{
     nom = nomU;
-
 }
 
-void PassarelaUsuari::posaCorreuElectronic(string correuU) {
-
+void PassarelaUsuari::posaCorreuElectronic(string correuU)
+{
     correuElectronic = correuU;
-
 }
 
-void PassarelaUsuari::posaDataNaixement(string neixU) {
-
+void PassarelaUsuari::posaDataNaixement(string neixU)
+{
     dataNaixement = neixU;
-
 }
---
+++ PassarelaUsuari.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include "ConnexioBD.h"
+
+using namespace std;
+
+const int SobrenomExisteix = 3;
+const int CorreuExisteix = 4;
+
+class PassarelaUsuari {
+public:
+    PassarelaUsuari(sql::ResultSet* result);
+    PassarelaUsuari();
+    PassarelaUsuari(string nomU, string sobrenomU, string contrasenyaU, string correuElectronicU, string dataNaixementU, string subscripcioU);
+    void insereix();
+    void modifica();
+    void esborra();
+
+    string obteContrasenya();
+    string obteNom();
+    string obteSobrenom();
+    string obteCorreuElectronic();
+    string obteDataNaixement();
+    void posaContrasenya(string contraU);
+    void posaNom(string nomU);
+    void posaCorreuElectronic(string correuU);
+    void posaDataNaixement(string neixU);
+
+private:
+    string nom, sobrenom, contrasenya, correuElectronic, dataNaixement, subscripcio;
+}; #pragma once
