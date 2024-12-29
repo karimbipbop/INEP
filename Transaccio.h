e60a9ff3e936c226e89aa7c12288cf5e9355a381
bielLacasa <biel.lacasa@estudiantat.upc.edu>
2024-12-29 02:25:04 +0100

    funcio today() feta

--- Transaccio.h
+++ Transaccio.h
@@ -10,6 +10,44 @@
 
 using namespace std;
 
+// Modifica la data que se li passa per canviar de DD/MM/YYYY a YYYY/MM/DD o al reves
+static bool formatDate(std::string& inputDate) {
+	std::vector<std::string> parts;
+	std::string part;
+
+	// Parse the input string and split it into parts
+	for (char ch : inputDate) {
+		if (ch == '/' || ch == '-') {
+			parts.push_back(part);
+			part.clear();
+		}
+		else if (ch == ' ')
+			break;
+		else {
+			part += ch;
+		}
+	}
+	parts.push_back(part); // Add the last part
+
+	// Validate that we have exactly three parts
+	if (parts.size() != 3) {
+		return false;
+	}
+
+	// Modify the input string to the formatted YYYY-MM-DD
+	inputDate = parts[2] + "/" + parts[1] + "/" + parts[0];
+	return true;
+}
+
+static string today() {
+	std::time_t t = std::time(nullptr);
+	std::tm* localTime = std::localtime(&t);
+	char buffer[11];
+	std::strftime(buffer, sizeof(buffer), "%Y-%m-%d", localTime);
+	std::string dataAvui = buffer;
+	return dataAvui;
+}
+
 const int ErrorContrasenya = 2;
 const int NoTancaSessio = 5;
 
